079cb9e2a743b7299fdc7da089f3bb7d62a0f88d
a-tsunoda <88137968+a-tsunoda@users.noreply.github.com>
2021-11-02 19:56:39 +0900

    1102

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "あああああ",
+    "いいいいいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["ううううううう"]);
   return (
     <>
-      <div>
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+      <div className="input-area">
+        <input id="add-text" placeholder="TODOを入力" />
+        <button id="add-button">追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
-        <ul>
-          <div>
-            <li>ああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+      <div className="imcomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul id="incomplete-list">
+          {incompleteTodos.map((todo) => {
+            return (
+              // mapなどでレンダリングするときはkeyを忘れずにつける
+              <li key={todo} className="list-row">
+                <p>{todo}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
-        <ul>
-          <div>
-            <li>いいいいいい</li>
-            <button>戻す</button>
-          </div>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul id="complete-list">
+          {completeTodos.map((todo) => {
+            return (
+              <li key={todo} className="list-row">
+                <p>{todo}</p>
+                <button>戻す</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
